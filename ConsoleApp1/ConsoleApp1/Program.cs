98a6860124024082b77528e501fedc2e30cd80b3
OttoAndersson <70747092+OttoAndersson@users.noreply.github.com>
2020-11-05 13:07:26 +0100

    yah

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,8 +8,34 @@ namespace ConsoleApp1
 {
     class Program
     {
+        static List<int> Swap(List<int>intlist)
+        {
+            for(int o=0; o < Math.Pow(intlist.Count, 2); o++)
+            {
+                for(int i = 0; i < intlist.Count; i++)
+                {
+                    if( i < intlist.Count - 1 && intlist[i] > intlist[i + 1])
+                    {
+                        int int_temp = intlist[i];
+                        intlist[i] = intlist[i + 1];
+                        intlist[i + 1] = int_temp;
+                    }
+                }
+            }
+            return intlist;
+        }
+
         static void Main(string[] args)
         {
+             List<int> sortlist = new List<int>() { 5, 7, 3, 6, 2, 9, 6, 5, 4, 1, 8, 9 };
+           
+            sortlist = Swap(sortlist);
+
+            for (int i = 0; i < sortlist.Count; i++)
+            {
+                Console.Write(sortlist[i]);
+            }
+            Console.ReadLine();
         }
     }
 }
